74a5221c0b0f150e6de4660e9b1655865d61d436
Ausianovich <49125846+Ausianovich@users.noreply.github.com>
2022-06-21 15:58:06 +0400

    Initial Commit

---
+++ TestLocalisationProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestLocalisationProject
+//
+//  Created by Kanstantsin Ausianovich on 21.06.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
